fefcbfd66ccaaec9d5401cc2b1f42bdfc85761c2
Peter Bailey <peter.eldridge.bailey@gmail.com>
2012-04-26 18:52:59 -0700

    one plot per system, with multiple rows per plot

--- turna.R
+++ turna.R
@@ -13,9 +13,9 @@ b = read.table('btable', sep=';', header=T)
 # with an id column for 'w', 'm', or 'b' for 'worst', 'median', and 'best',
 # respectively.
 
-w$type = 'w'
-b$type = 'b'
-m$type = 'm'
+w$type = 'Worst'
+b$type = 'Best'
+m$type = 'Median'
 
 a = rbind(w,m,b)
 a = a[order(a$App, a$System),]
@@ -28,20 +28,20 @@ otherApps = c("Sparse","SMG2000","Sweep3D","LAMMPS","UMT2k")
 appList = c(nasApps, otherApps)
 
 # shorten system names
-a$System = factor(toupper(substr(a$System, 1, 1)))
+#a$System = factor(toupper(substr(a$System, 1, 1)))
 a$Total = a$Execution + a$Queueing_Delay
 
 # scale to CC8 times
 sa = a
 for(row in 1:length(a$App)){
-	 denom_sel = which(sa$App == sa$App[row] & sa$System == 'C' & sa$type == sa$type[row])
+	 denom_sel = which(sa$App == sa$App[row] & sa$System == 'cc2.8xlarge' & sa$type == sa$type[row])
 	 sa$Queueing_Delay[row] = sa$Queueing_Delay[row] / sa$Total[denom_sel]
 	 sa$Execution[row] = sa$Execution[row] / sa$Total[denom_sel]
 	 sa$Ratio[row] = sa$Total[row] / sa$Total[denom_sel]
 }
 
 # remove CC8 times
-sa = sa[sa$System != 'C',]
+sa = sa[sa$System != 'cc2.8xlarge',]
 
 systems = unique(sa$System)
 numApps = length(appList)
@@ -53,47 +53,27 @@ mOther = sa[sa$App %in% otherApps,]
 # NAS apps
 
 #mNAS = melt(mNAS[,c('App','System','type','Execution','Queueing_Delay')], id.vars=c('App','System','type'))
-mNAS = melt(mNAS[,c('App','System','type','Ratio')], id.vars=c('App','System','type'))
 
-# log scale
-#mNAS$value = log10(mNAS$value)
+for(system in systems){
+    msa = melt(sa[sa$System == system,c('App','type','Ratio')], id.vars=c('App','type'))
 
-mNAS$System = paste(as.character(mNAS$System), mNAS$type, sep='-')
-mNAS = mNAS[,!(names(mNAS) %in% 'type')]
+    # log scale
+    #msa$value = log10(msa$value)
 
-ggplot(mNAS, aes(x=System, y=value, fill=variable)) +
-       ylab("Log seconds") + 
-       xlab("System-{Worst, Median, Best}") + 
-       facet_grid(. ~ App) +
+    #msa$System = paste(as.character(msa$System), msa$type, sep='-')
+    #msa = msa[,!(names(msa) %in% 'type')]
+
+    ggplot(msa, aes(x=App, y=value, fill=variable)) +
+       ylab("Seconds") + 
+       xlab("App") + 
+       facet_grid(type ~ ., scales="free") +
        scale_fill_manual(values = c('grey40','white')) +
-       opts(panel.background = theme_rect(fill = 'white', colour = NA)) +
+#       opts(panel.background = theme_rect(fill = 'white', colour = NA)) +
        opts(title = expression('Applications')) +
        opts(legend.position = 'top') + 
        opts(fontsize = 5) + 
        geom_bar(colour = 'black', width = .6)
 
-ggsave(filename="NAS.eps", width=17.5, height=6)
-
-# other apps
-
-mOther = melt(mOther[,c('App','System','type','Execution','Queueing_Delay')], id.vars=c('App','System','type'))
-
-# log scale
-#mOther$value = log10(mOther$value)
-
-mOther$System = paste(as.character(mOther$System), mOther$type, sep='-')
-mOther = mOther[,!(names(mOther) %in% 'type')]
-
-ggplot(mOther, aes(x=System, y=value, fill=variable)) +
-       ylab("Log seconds") + 
-       xlab("System-{Worst, Median, Best}") + 
-       facet_grid(. ~ App) +
-       scale_fill_manual(values = c('grey40','white')) +
-       opts(panel.background = theme_rect(fill = 'white', colour = NA)) +
-       opts(title = expression('Applications')) +
-       opts(legend.position = 'top') + 
-       opts(fontsize = 5) + 
-       geom_bar(colour = 'black', width = .6)
-
-ggsave(filename="other.eps", width=17.5, height=6)
+    ggsave(filename=paste(system, "eps", sep='.'), width=17.5, height=6)
+}
 
